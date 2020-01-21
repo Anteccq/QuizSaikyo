2c58f251998d81a9846266365174fc086d0938a7
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2020-01-21 18:17:28 +0900

    add ViewModel

---
+++ QuizSaikyo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+using QuizSaikyo.Models;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace QuizSaikyo.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public SerialManager SerialManager { get; } = new SerialManager();
+
+        public ReadOnlyReactiveProperty<int> SerialByte { get; }
+
+        public MainPageViewModel() => SerialByte = SerialManager.ObserveProperty(x => x.NextData).ToReadOnlyReactiveProperty();
+    }
+}
