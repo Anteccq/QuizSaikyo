28212f72f25bb95b18117f00ae91913cb0240393
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2020-01-21 17:43:01 +0900

    Initial commit

---
+++ QuizSaikyo/Models/SerialManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace QuizSaikyo.Models
+{
+    public class SerialManager : BindableBase
+    {
+        private readonly SerialPort serialPort = new SerialPort("COM3", 9600);
+
+        private int _nextData;
+        public int NextData
+        {
+            get => _nextData;
+            set => SetProperty(ref _nextData, value);
+        }
+
+        public SerialManager()
+        {
+            try
+            {
+                serialPort.NewLine = "\r\n";
+                serialPort.Open();
+                serialPort.DataReceived += (sender, e) => { NextData = ((SerialPort) sender).ReadByte(); };
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public void PortDispose()
+        {
+            if(serialPort.IsOpen) serialPort.Close();
+            serialPort.Dispose();
+        }
+    }
+}
